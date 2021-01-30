d05fbf73f1424308348708f5449cdf6a7eeadc47
unknown <jasmineakbari@yahoo.com>
2021-01-30 13:14:17 -0500

    added ability to create/update/delete posts

--- models/index.js
+++ models/index.js
@@ -1,4 +1,13 @@
 const User = require('./User');
 const Post = require("./Post");
 
+// create associates between posts and users
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+});
+
 module.exports = { User, Post };
