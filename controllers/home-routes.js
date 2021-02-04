c0763f614b411fa1886190eddcfe23b98dcfc58b
unknown <jasmineakbari@yahoo.com>
2021-02-04 17:47:25 -0500

    added conditional render and if statments to display login and logout

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -31,7 +31,11 @@ router.get('/', (req, res) => {
         .then(dbPostData => {
             // pass a single object into the homepage template
             const posts = dbPostData.map(post => post.get({ plain: true }));
-            res.render('homepage', { posts });
+            
+            res.render('homepage', {
+                posts,
+                loggedIn: req.session.loggedIn
+            });
         })
         .catch(err => {
             console.log(err);
