ba348e4e3b8bd198d53c00af94029a551afa9ce8
unknown <jasmineakbari@yahoo.com>
2021-02-07 13:09:22 -0500

    created comment dashboard w create delete and edit

--- _tests_/helpers.test.js
+++ _tests_/helpers.test.js
@@ -6,6 +6,14 @@ test('format_date() returns a date string', () => {
     expect(format_date(date)).toBe('3/20/2020');
 });
 
+test('format_plural() returns a pluralized word', () => {
+    const word1 = format_plural('tiger', 1);
+    const word2 = format_plural('lion', 2);
+  
+    expect(word1).toBe('tiger');
+    expect(word2).toBe('lions');
+});
+
 test('format_url() returns a simplified url string', () => {
     const url1 = format_url('http://test.com/page/1');
     const url2 = format_url('https://www.coolstuff.com/abcdefg/');
